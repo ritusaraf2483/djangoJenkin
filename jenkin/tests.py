24baa8c451c478a746b1d869a7ae26f6865daf20
ritusaraf2483 <ritusaraf2483@gmail.com>
2022-03-10 13:59:53 -0600

    ..

--- jenkin/tests.py
+++ jenkin/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.http import response
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_admin_page_status_code(self):
+        print("Verify admin Page")
+        response=self.client.get('/admin')
+        self.assertEquals(response.status_code,301)
+
+    def test_about_page_status_code(self):
+        print("Verify not exit about Page")
+        response = self.client.get('/about')
+        self.assertEquals(response.status_code, 404)
