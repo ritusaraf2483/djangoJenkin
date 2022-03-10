7e38a29b7ad8b57a2d2cc04751aa9cca5e21bd07
ritusaraf2483 <ritusaraf2483@gmail.com>
2022-03-10 13:54:51 -0600

    ..

--- jenkin/views.py
+++ jenkin/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
-from django.http import response
-from django.test import SimpleTestCase
 
-class HomePageTests(SimpleTestCase):
-    def test_admin_page_status_code(self):
-        print("Verify admin Page")
-        response=self.client.get('/admin')
-        self.assertEquals(response.status_code,301)
-
-    def test_about_page_status_code(self):
-        print("Verify not exit about Page")
-        response = self.client.get('/about')
-        self.assertEquals(response.status_code, 404)
 # Create your views here.
